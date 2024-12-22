5f08dda3748ef0cdf92f9bc4fcbbbd8c1d95ab64
MD ANIKUL ISLAM <anikul.islam01@northsouth.edu>
2024-12-22 15:12:15 +0600

    parallex effect add in object image

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,4 +8,20 @@
   $(".sidebar-close-btn, .mobile-menu-overlay").on("click", function () {
     $(".navbar-collapse, .mobile-menu-overlay").removeClass("open");
   });
+
+  // Parallax scroll effect js
+  $(".move-with-cursor").each(function () {
+    var $this = $(this);
+    $(document).on("mousemove", function (e) {
+      var t = e.clientX,
+        e = e.clientY;
+      $this.css({
+        transition: "transform 0.6s cubic-bezier(0.34, 1.56, 0.64, 1)",
+        transform: `translate(${0.01 * t}px, ${0.01 * e}px) rotate(${
+          0.01 * (t + e)
+        }deg)`,
+      });
+    });
+  });
+  
 })(jQuery);
